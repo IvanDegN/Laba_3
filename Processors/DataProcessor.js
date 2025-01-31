b82a0e2d811701d4238b50d1838c768b8cbf9937
Ivan <mauzer.bruno@yandex.ru>
2025-01-31 16:20:45 +0300

    create method for random generated number

--- Processors/DataProcessor.js
+++ Processors/DataProcessor.js
@@ -7,4 +7,27 @@ export class DataPocessor {
         this.chartCanvas = document.getElementById(chartCanvasId);
         this.chartInstance = null;
     }
+
+
+// Общий метод для чтения файла
+readFile() {
+    return new Promise((resolve, reject) => {
+        if (!this.fileInput.files.length) {
+            alert('Выберите файл с данными.');
+            reject();
+            return;
+        }
+        const file = this.fileInput.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => resolve(e.target.result);
+        reader.onerror = () => reject(reader.error);
+        reader.readAsText(file);
+    });
+}
+
+// Метод для генерации случайного цвета
+getRandomColor(){
+    return `rgba(${Math.floor(Math.random() * 256 )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256 )}, 1)`;
+}
+
 }

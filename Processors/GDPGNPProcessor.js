8a1c0f7db7b17eb7ce1ecc1ddd3470a2efec80a3
Ivan <mauzer.bruno@yandex.ru>
2025-01-31 16:54:31 +0300

    creating a method for parsing data

--- Processors/GDPGNPProcessor.js
+++ Processors/GDPGNPProcessor.js
@@ -6,5 +6,20 @@ export class GDPGNPProcessor extends DataProcessor{
     constructor(fileInputId, outputDivId, chartCanvasId){
         super(fileInputId, outputDivId, chartCanvasId);
     }
+
+
+// Парсинг данных
+parseData(text){
+    const rows = text.trim().split('\n');
+    const headers = rows[0].split(/\s+/);
+    return rows.slice(1).map(row => {
+        const values = row.split(/\s+/);
+        return headers.reduce((obj, header, index) =>{
+            obj[header.trim()] = values[index]?.trim() || '';
+            return obj;
+        }, {} );
+    });
+}
+
 }
 
